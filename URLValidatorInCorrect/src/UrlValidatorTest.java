e25775b9ce1e83d21a8ac1d45aeb5535b9ce46fc
Bryce Holewinski <bryce.holewinski@gmail.com>
2017-08-12 08:35:58 -0700

    added 5th partition test

--- URLValidatorInCorrect/src/UrlValidatorTest.java
+++ URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -37,8 +37,8 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
+
+
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
@@ -49,7 +49,7 @@ public class UrlValidatorTest extends TestCase {
 	   System.out.println(urlVal.isValid("https://piazza.com/class/j4bo6gz138r46l?cid=158"));
 	   System.out.println(urlVal.isValid("h3t://256.256.256.256:65535/$23?action=edit&mode=up"));
 	   System.out.println(urlVal.isValid("http://www.google.com:80/$23"));
-	   
+
 	   //test for valid and invalid ASCII characters
 	   assertTrue(urlVal.isValid("http://www.amazon.com"));
 	   assertFalse(urlVal.isValid("http://www.amäzon.com"));
@@ -57,13 +57,13 @@ public class UrlValidatorTest extends TestCase {
 	   assertFalse(urlVal.isValid("https://www.fäcebook.com"));
 	   assertTrue(urlVal.isValid("http://oregonstate.edu/"));
 	   assertFalse(urlVal.isValid("http://orëgonstate.edu/"));
-	   
+
 	   //test for valid and invalid schemes
 	   assertTrue(urlVal.isValid("https://github.com/"));
 	   assertTrue(urlVal.isValid("http://github.com/"));
 	   //assertFalse(urlVal.isValid("htstps://github.com/")); //bug here - test fails, uncomment to verify
 	   //assertFalse(urlVal.isValid("ttps://github.com/")); //bug here - test fails, uncomment to verify
-	   
+
 	   //test for valid and invalid domain names
 	   assertTrue(urlVal.isValid("http://www.public.navy.mil/spawar/Pages/default.aspx"));
 	   assertTrue(urlVal.isValid("https://www.nasa.gov/"));
@@ -73,23 +73,23 @@ public class UrlValidatorTest extends TestCase {
 	   //assertTrue(urlVal.isValid("www.spiegel.de/international/")); //bug here - test fails, uncomment to verify
 	   //assertTrue(urlVal.isValid("http://localhost:400/")); //bug here - test fails, uncomment to verify
    }
-   
+
    /*****************************Testing Scheme. *****************************/
    public void testYourFirstPartition()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	
+
 	   String baseURL = "www.amazon.com";
-	 
+
 	   String testUrl = "https://" +baseURL;
 	   System.out.println("Testing Valid Scheme : ");
 	   boolean result = urlVal.isValid(testUrl);
-	   
-	   System.out.println("Expected Result : TRUE    Actual Result : "+result); 
-		   
+
+	   System.out.println("Expected Result : TRUE    Actual Result : "+result);
+
 
 	   String[] inValidScheme = {"htsp://", "hts://", "ht://",  "h://"};
-	 
+
 	   System.out.println("Testing Invalid Scheme : ");
 	   result = false; //these tests should return false
 	   for (int i = 0; i <= inValidScheme.length ; i++)
@@ -103,30 +103,30 @@ public class UrlValidatorTest extends TestCase {
    }
    /*****************************Testing Authority *****************************/
    public void testYourSecondPartition(){
-   
-	   
-	   
-	   
-	   
-	   
-	   
+
+
+
+
+
+
+
    }
    /*****************************Testing Authority *****************************/
    public void testYourThirdPartition(){
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-		
+
 	   String baseURL = "https://www.amazon.com";
-	 
+
 	   String testUrl =  baseURL + ":80";
 	   System.out.println("Testing Valid Scheme : ");
 	   boolean result = urlVal.isValid(testUrl);
-	   
-	   System.out.println("Expected Result : TRUE    Actual Result : "+result); 
-		   
-	   Random rand = new Random();
-	  
 
-	 
+	   System.out.println("Expected Result : TRUE    Actual Result : "+result);
+
+	   Random rand = new Random();
+
+
+
 	   System.out.println("Testing Invalid Scheme : ");
 	   //result = false; //these tests should return false
 	   for (int i = 0; i < 1000 ; i++)
@@ -137,48 +137,65 @@ public class UrlValidatorTest extends TestCase {
 		   result = (urlVal.isValid(testUrl));
 		   if(!result)
 		   System.out.println(n + "is invalid port");
-		  
-			   
+
+
 	   }
-	   
+
    }
    /*****************************Testing Path *****************************/
    public void testYourFourthPartition(){
-   
-	   
-	   
-	   
-	   
-	   
-	   
+
+
+
+
+
+
+
    }
-   
+
    /*****************************Testing Query *****************************/
    public void testYourFifthPartition(){
-	   
-   
+     UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+	   String baseURL = "http://www.google.com/";
+
+	   String testUrl = baseURL;
+	   System.out.println("Testing the base URL for the Valid Query test: ");
+	   boolean result = urlVal.isValid(testUrl);
+
+	   System.out.println("Expected Result : TRUE    Actual Result : "+result);
+
+
+	   String[] validQuery = {"search?q=junit&oq=junit&gs_l", "search?q=eclipse+ide&oq=eclipse+ide&gs_l=psy-ab.3", "search?q=how+to+write+bug+reports&oq=how+to+write+bug+reports&gs_l=psy-ab.3..0j0i22i30k1l3.81686.92729.0.93106.44.34.10.0.0.0.167.3411.22j12.34.0....0...1.1.64.psy-ab..0.44.3455...35i39k1j0i131k1j0i67k1j0i20k1.KUpHDCRFO00"};
+
+	   System.out.println("Testing Valid Query : ");
+	   result = true; //these tests should return true
+	   for (int i = 0; i <= validQuery.length ; i++)
+	   {
+		    testUrl += validQuery[i];
+		   result = (urlVal.isValid(testUrl));
+		   if(result)
+		   System.out.println("Expected Result : TRUE    Actual Result : "+result);
+	   }
+
    }
-   
-   
-   
-   
+
+
+
+
    public void testIsValid()
    {
 	   for(int i = 0;i<10000;i++)
 	   {
-		   
+
 	   }
    }
-   
+
    public void testAnyOtherUnitTest()
    {
-		
+
    }
 
- 
-       
+
+
 }
-
-
-
-
